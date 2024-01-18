a55fc8f257c158b991b5c72ba56a25ee152a00b2
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2024-01-18 22:12:50 +0200

    SquareParallelBarPattern

---
+++ src/SolveAnyAquareOrHollowPattern/SquareParallelBarPattern.java
@@ -0,0 +1,25 @@
+package SolveAnyAquareOrHollowPattern;
+
+import java.util.Scanner;
+
+public class SquareParallelBarPattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number: ");
+        int n = scanner.nextInt();
+
+        // First Column: cols = 1
+        // Last Column: cols = n
+        for (int rows = 1; rows <= n; rows++) {
+            for (int cols = 1; cols <= n; cols++) {
+                if (cols == 1 || cols == n) {
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
